a2527a8f178eee3eb1e83904f99e04c4ba3570e9
Sofiya Polienko <sofie.polienko@gmail.com>
2021-04-10 01:01:17 +0300

    feat: add task 03

--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -9,8 +9,43 @@
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new Error('Not implemented');
+
+function searchMinusOne(array) {
+  const arrayMinusOne = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === -1) {
+      arrayMinusOne.push(i);
+    }
+  }
+  return arrayMinusOne;
+}
+
+function sortNum(a, b) {
+  return a - b;
+}
+
+function addMinusOne(array, indexArray) {
+  array.sort(sortNum);
+  for (let i = 0; i < indexArray.length; i++) {
+    array.splice(indexArray[i], 0, -1);
+  }
+}
+
+function removeMinusOne(array, num) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === num) {
+      array.splice(i, 1);
+      i--;
+    }
+  }
+}
+
+function sortByHeight(arr) {
+  const array = arr;
+  const arrayMinusOne = searchMinusOne(array);
+  removeMinusOne(array, -1);
+  addMinusOne(array, arrayMinusOne);
+  return array;
 }
 
 module.exports = sortByHeight;
