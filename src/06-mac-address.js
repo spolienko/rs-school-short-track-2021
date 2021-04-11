3387431b40e0241fe7acc1db8a6e154bc306a8ae
Sofiya Polienko <sofie.polienko@gmail.com>
2021-04-11 13:53:00 +0300

    feat: task 06

--- src/06-mac-address.js
+++ src/06-mac-address.js
@@ -12,8 +12,10 @@
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new Error('Not implemented');
+
+function isMAC48Address(n) {
+  const rex = /(?<!-)\b(?:[0-9A-Fa-f]{2}-){5}[0-9A-Fa-f]{2}\b(?!:)/;
+  return rex.test(n);
 }
 
 module.exports = isMAC48Address;
